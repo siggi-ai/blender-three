7318dc403bd58bc22e33ee187b998bb242affccc
siggi <siegfried.hamm@gmail.com>
2022-07-18 14:01:14 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Three msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Three from './components/Three.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Three
   }
 }
 </script>
